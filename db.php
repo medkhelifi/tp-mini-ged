9466ba3d7aaa6ec7be34a5e28701a0354de39af2
mohamedchrif.khelifi@gmail.com <T#VoOn1Gl$ULG75>
2015-10-20 12:32:21 +0100

    db login ...

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=tp_miniged;charset=utf8', 'root', '');
+    $hostname   = 'localhost';
+    $database   = 'tp_miniged';
+    $username   = 'root';
+    $password   = '';
+    //$bdd        = new PDO('mysql:host=localhost;dbname=tp_miniged;charset=utf8', 'root', '');
+    $bdd        = new PDO('mysql:host='.$hostname.';dbname='.$database.';charset=utf8', ''.$username.'', ''.$password.'');
 }
 catch (Exception $e)
 {
--- login.php
+++ login.php
@@ -1,33 +1,33 @@
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-info" >
-            <div class="panel-heading">
-                <div class="panel-title">Connexion</div>
-            </div>
+<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div class="panel panel-info" >
+        <div class="panel-heading">
+            <div class="panel-title">Connexion</div>
+        </div>
 
-            <div style="padding-top:30px" class="panel-body" >
-                <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                <form id="loginform" class="form-horizontal" method="post" role="form">
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-email" type="text" class="form-control" name="email" value="" placeholder="E-Mail">
+        <div style="padding-top:30px" class="panel-body" >
+            <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+            <form id="loginform" class="form-horizontal" method="post" role="form">
+                <div style="margin-bottom: 25px" class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    <input id="login-email" type="text" class="form-control" name="email" value="" placeholder="demo">
+                </div>
+                <div style="margin-bottom: 25px" class="input-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                    <input id="login-password" type="password" class="form-control" name="password" placeholder="demo">
+                </div>
+                <div class="input-group">
+                    <div class="checkbox">
+                        <label>
+                            <input id="login-remember" type="checkbox" name="remember" value="1"> Se souvenir de moi
+                        </label>
                     </div>
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password" placeholder="Mot de passe">
+                </div>
+                <div style="margin-top:10px" class="form-group">
+                    <div class="col-sm-12 controls">
+                        <button type="submit" class="btn btn-success">Connexion</button>
                     </div>
-                    <div class="input-group">
-                        <div class="checkbox">
-                            <label>
-                                <input id="login-remember" type="checkbox" name="remember" value="1"> Se souvenir de moi
-                            </label>
-                        </div>
-                    </div>
-                    <div style="margin-top:10px" class="form-group">
-                        <div class="col-sm-12 controls">
-                            <button type="submit" class="btn btn-success">Connexion</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
